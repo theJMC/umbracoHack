dbc37dee72344a73c5695ce1044834e06afd1333
JesssseL <146840057+JesssseL@users.noreply.github.com>
2025-11-28 12:24:26 +0000

    add base controller for services

---
+++ UmbracoHack25/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace UmbracoHack25.Controllers
+{
+    public class SupportServicesController : RenderController
+    {
+        public SupportServicesController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor)
+            : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+        }
+        public override IActionResult Index()
+        {
+            var foo = "bar";
+            return CurrentTemplate(CurrentPage);
+        }
+    }
+}
